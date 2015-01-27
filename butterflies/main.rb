f773c81a08ae997fac94671b2c1d5f54bd31c9df
王雪梅 <wxm112@me.com>
2015-01-27 14:56:17 +1100

    added butterflier project

---
+++ butterflies/main.rb
@@ -0,0 +1,43 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+
+require 'pry'
+
+get '/' do
+  erb :home
+end
+
+get '/butterflies' do
+  @butterflies = db_query("SELECT * FROM butterflies")
+  erb :index
+end
+
+get '/butterflies/new' do
+ erb :new
+end
+
+
+post '/butterflies' do
+    "This seems to be working"
+end
+
+get '/butterflies/:id' do
+  id = params[:id]
+
+  @butterfly = db_query "SELECT * FROM butterflies WHERE id = #{id}"
+  @butterfly = @butterfly.first
+  erb :show
+end
+
+
+
+
+def db_query(sql)
+  db = SQLite3::Database.new 'butterflies.db'
+  db.results_as_hash = true
+
+  result = db.execute sql
+  db.close
+  result
+end
