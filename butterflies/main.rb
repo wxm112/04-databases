b0f691a25618191aba16fe96dba980e24d88a3d5
王雪梅 <wxm112@me.com>
2015-01-27 16:38:40 +1100

    finished butterflies project

--- butterflies/main.rb
+++ butterflies/main.rb
@@ -4,6 +4,10 @@ require 'sqlite3'
 
 require 'pry'
 
+before do
+  @families = db_query("SELECT DISTINCT(family) FROM butterflies")
+end
+
 get '/' do
   erb :home
 end
@@ -13,31 +17,66 @@ get '/butterflies' do
   erb :index
 end
 
-get '/butterflies/new' do
- erb :new
+get '/butterflies/family/:name' do
+  @butterflies = db_query("SELECT * FROM butterflies WHERE family='#{params[:name]}'")
+  erb :index
 end
 
+get '/butterflies/new' do
+  erb :new
+end
 
 post '/butterflies' do
-    "This seems to be working"
+  query = "INSERT INTO butterflies (name, family, image)
+           VALUES ('#{params["name"]}', '#{params["family"]}',
+                   '#{params["image"]}')"
+
+  db_query(query)
+
+  redirect to('/butterflies')
 end
 
 get '/butterflies/:id' do
   id = params[:id]
 
-  @butterfly = db_query "SELECT * FROM butterflies WHERE id = #{id}"
+  @butterfly = db_query("SELECT * FROM butterflies WHERE id = #{ id }")
   @butterfly = @butterfly.first
+
   erb :show
 end
 
+get '/butterflies/:id/delete' do
+  id = params[:id]
 
+  db_query("DELETE FROM butterflies WHERE id = #{ id }")
 
+  redirect to('/butterflies')
+end
+
+get '/butterflies/:id/edit' do
+  id = params[:id]
+
+  @butterfly = db_query("SELECT * FROM butterflies WHERE id = #{ id }").first
+
+  erb :edit
+end
+
+post '/butterflies/:id' do
+  id = params[:id]
+
+  query = "UPDATE butterflies SET name='#{params["name"]}', family='#{params["family"]}', image='#{params["image"]}' WHERE id = #{id}"
+
+  db_query(query)
+
+  redirect to('/butterflies/' + id)
+end
 
 def db_query(sql)
-  db = SQLite3::Database.new 'butterflies.db'
+  db = SQLite3::Database.new "butterflies.db"
   db.results_as_hash = true
 
   result = db.execute sql
+
   db.close
   result
 end
